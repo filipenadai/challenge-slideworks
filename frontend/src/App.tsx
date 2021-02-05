d2f09fc4b49295f5b9afa0b2ea8a41e4e5c2e3fa
Filipe Nadai <f.nadai05@gmail.com>
2021-02-04 22:00:12 -0300

    style: initial page styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import FormPage from './pages/FormPage';
 
-const App: React.FC = () => <div className="App">Lucas</div>;
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <GlobalStyle />
+    <FormPage />
+  </>
+);
 
 export default App;
---
+++ frontend/src/pages/FormPage/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const FormPage: React.FC = () => {
+  return (
+    <Container>
+      <div>Initial page</div>
+    </Container>
+  );
+};
+
+export default FormPage;
